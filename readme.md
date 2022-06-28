0b11150db9e1c775d151480ddcd8533fed032f0f
unknown <ienulesccatalin@gmail.com>
2022-06-28 15:49:27 +0300

    add all my files

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Dictionary API
+
+I created a dictionary app where you can search for words and it outputs word meaning, word pronunciation and usage example.
+
+## Description
+
+At Javascript chapter during the Web Development course at ITSchool I had to do a dictionary app using API.
+
+## Features
+
+- API
+- Responsive app
+- Show word meaning, word pronunciation and usage example
+
+## Link Example
+
+Link to [Dictionary API - App](https://creative-dictionary-api.netlify.app/)
---
+++ scripts.js
@@ -0,0 +1,33 @@
+let result = document.querySelector(".result");
+let btn = document.querySelector("#btn-search");
+
+btn.addEventListener("click", () => {
+  let inputWord = document.querySelector("#word-input").value;
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`).then(
+    (response) => {
+      response
+        .json()
+        .then((data) => {
+          console.log(data);
+          result.innerHTML = `
+        <div class="word">
+          <h3>${inputWord}</h3>
+        </div>
+        <div class="details">
+          <p>${data[0].meanings[0].partOfSpeech}</p>
+          <p>${data[0].phonetic}/</p>
+        </div>
+        <p class="word-meaning">
+        ${data[0].meanings[0].definitions[0].definition}
+        </p>
+        <p class="word-example">
+        ${data[0].meanings[0].definitions[0].example || ""}
+        </p>
+        `;
+        })
+        .catch(() => {
+          result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+    }
+  );
+});
